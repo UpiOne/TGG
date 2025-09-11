713fbb554604e4e9861710f8454869ff6cf7642d
UpiOne <122791618+UpiOne@users.noreply.github.com>
2025-09-11 19:11:50 +0300

    313131

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,6 +1,16 @@
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
-    
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[Service Worker] Activate');
+    e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function (e) {
+    // Простая обработка fetch событий
+    e.respondWith(fetch(e.request));
 });
 
